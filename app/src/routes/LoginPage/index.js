06a5059da68f5df3db20552834a52f437308a710
Rosana Rezende <contato@rosanarezende.com>
2020-07-03 11:21:36 -0300

    layout login

--- app/src/routes/LoginPage/index.js
+++ app/src/routes/LoginPage/index.js
@@ -7,8 +7,26 @@ function LoginPage(){
     return (
         <>
         <S.Box>
+            <S.Logo 
+                src="https://user-images.githubusercontent.com/45580434/86472595-c2e56d80-bd15-11ea-9794-f11f547dc2d3.png"
+                alt="logo"
+            />
+            <S.Form>
+                <S.Label for="email">
+                    <img src="https://user-images.githubusercontent.com/45580434/86475683-83ba1b00-bd1b-11ea-92d0-474cddd5f7c8.png" alt="email"/>
+                </S.Label>
+                <S.Input type="email" id="email"/>
 
-        LoginPage
+                <S.Label for="password">
+                    <img src="https://user-images.githubusercontent.com/45580434/86475686-861c7500-bd1b-11ea-925b-6589efdfc96b.png" alt="senha"/>
+                </S.Label>
+                <S.Input type="password" id="password"/>
+
+                <S.Button>
+                    {/* Entrar */}
+                    <img src="https://user-images.githubusercontent.com/45580434/86477035-07750700-bd1e-11ea-8df9-08b766481d68.png" alt="entrar"/>
+                </S.Button>
+            </S.Form>
         </S.Box>
         </>
     )
--- app/src/routes/LoginPage/styles.js
+++ app/src/routes/LoginPage/styles.js
@@ -2,11 +2,66 @@ import styled from "styled-components"
 
 export const Box = styled.div`
     position: absolute;
-    width: 448px;
-    height: 408px;
-    left: 416px;
-    top: 156px;
+    width: 35vw;
+    height: 56.6vh;
+    left: 32.5vw;
+    top: 21.7vh;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
     border: 1px solid #212121;
     box-sizing: border-box;
+`
+
+export const Logo = styled.img`
+    margin-top: 10%;
+`
+
+export const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    width: 85%;
+    height: 55%;
+    margin: auto;
+`
+
+export const Label = styled.label`
+    height: 18px;
+
+    font-family: Montserrat;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 18px;
+
+    display: flex;
+    align-items: center;
+
+
+    color: #212121;
+    margin-bottom: 1%;
+`
+
+export const Input = styled.input`
+    height: 17.5%;
+    margin-bottom: 8.5%;
+
+    background: #FFFFFF;
+    border: 1px solid #424242;
+    box-sizing: border-box;
+`
+
+export const Button = styled.button`
+    height: 17.5%;
+    background: #212121;
+
+    font-family: Montserrat;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 24px; 
+    color: #FFFFFF;
+    border: none;
 `
